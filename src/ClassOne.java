c1046b357408a68700c67e69de27150f5aaa0ce2
Nur Tırnova <nur.sagliksever@gmail.com>
2021-08-07 16:27:25 +0300

    third commit

--- src/ClassOne.java
+++ src/ClassOne.java
@@ -3,5 +3,6 @@ public class ClassOne {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Batch 35/36 learn Git");
+        System.out.println("We Like Git but we will like more when we have more practice");
     }
 }
