93c39b3bf1bc6b8186285facd21890aea709840e
Nur Tırnova <nur.sagliksever@gmail.com>
2021-08-07 22:34:56 +0300

    adding line 8 locally

--- src/ClassThree.java
+++ src/ClassThree.java
@@ -1,13 +1,13 @@
 public class ClassThree {
-
     public static void main(String[] args) {
         int x = 5;
         int sum = x+2;
         System.out.println(sum);
         
         int y = 0;
-        
+        String str = "Java";
+
         System.out.println("THIS IS REMOTE REPO CODE");
-        System.out.println("THIS IS REMOTE REPO CODE TRY 2");
+
     }
 }
